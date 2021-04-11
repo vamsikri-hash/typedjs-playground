04f0ce90dcc8346ca02f194beabc7f8aac51bd0c
vamsi <bellamvamsikrishna3415@gmail.com>
2021-04-11 18:14:46 +0530

    Tried some dom in ts

--- DOM/index.js
+++ DOM/index.js
@@ -1,10 +1,30 @@
-var email;
-var password;
-var submit;
-email = document.getElementById("email");
-password = document.getElementById("password");
-submit = document.getElementById("submit");
+// why its showing error when using HTMLInputElement (which is actually a input element) - caught as typescript onlu knows that documnet.anyquery will return HTMLElement
+// but we may know it can be input or button or anchor any thing so we should use type assertions provided by ts to get exact types.
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var submit = document.getElementById("submit");
+var generateAnchors = document.getElementById("generate-anchors");
+var anchorElements = document.querySelector(".anchor-elements");
 submit.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(email.value, password.value);
 });
+generateAnchors.addEventListener("click", function () {
+    console.log("hello");
+    var listTag = document.createElement("ol"); //  It's HTMLOList but i given UList still taken
+    for (var i = 0; i < 10; i++) {
+        var listItemTag = document.createElement("li");
+        listItemTag.appendChild(randomAnchor(i));
+        listTag.appendChild(listItemTag);
+    }
+    ;
+    anchorElements.appendChild(listTag);
+});
+var randomAnchor = function (i) {
+    var aTag = document.createElement("a");
+    aTag.appendChild(document.createTextNode("link" + i));
+    aTag.href = "https://localhost:3000/" + i;
+    aTag.title = "link" + i;
+    return aTag;
+};
+// try for other html types anchor, list, other events etc
--- DOM/index.ts
+++ DOM/index.ts
@@ -8,11 +8,36 @@
 const email: HTMLInputElement = document.getElementById("email") as HTMLInputElement;
 const password: HTMLInputElement = document.getElementById("password") as HTMLInputElement;
 const submit: HTMLButtonElement = document.getElementById("submit") as HTMLButtonElement;
+const generateAnchors: HTMLButtonElement = document.getElementById("generate-anchors") as HTMLButtonElement;
+const anchorElements: HTMLDivElement = document.querySelector(".anchor-elements");
 
-submit.addEventListener("click",(e: MouseEvent) => {
+submit.addEventListener("click",(e: MouseEvent): void => {
       e.preventDefault();
       console.log(email.value,password.value);
 });
+
+generateAnchors.addEventListener("click", (): void => {
+      console.log("hello");
+      const listTag: HTMLUListElement = document.createElement("ol"); //  It's HTMLOList but i given UList still taken
+      for(let i=0;i<10;i++) {
+            const listItemTag: HTMLLIElement = document.createElement("li");
+            listItemTag.appendChild(randomAnchor(i));
+            listTag.appendChild(listItemTag);
+      };
+      anchorElements.appendChild(listTag);
+})
+
+
+
+const randomAnchor = (i: number): HTMLAnchorElement => {
+      const aTag = document.createElement("a");
+      aTag.appendChild(document.createTextNode(`link${i}`));
+      aTag.href = `https://localhost:3000/${i}`;
+      aTag.title = `link${i}`;
+      return aTag as HTMLAnchorElement;
+};
+
+
 
 
 // try for other html types anchor, list, other events etc
